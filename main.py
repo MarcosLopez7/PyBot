10dd6f75318e5ca9ee2e489f8177fb768d7e9993
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-09-21 16:15:02 -0500

    message reply

--- main.py
+++ main.py
@@ -11,4 +11,13 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    await message.channel.send("Hola has enviado un mensaje")
+
+
 client.run(TOKEN)
