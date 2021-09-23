cefd23590330f1f3342f3f6838e7de9cc3ab7a07
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-09-22 21:54:16 -0500

    Creado comando para setear canal de bienvenida

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
-
 import discord
+
+from DBManager import DBManager
 
 client = discord.Client()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+db_manager = DBManager()
+conn = db_manager.get_conn()
 
 
 @client.event
@@ -14,10 +18,45 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    if message.content == '$set_welcome_channel':
+        roles = message.author.roles
 
-    await message.channel.send("Sola has enviado un mensaje")
+        for role in roles:
+            if role.permissions.administrator:
+                cursor = conn.cursor()
+
+                cursor.execute("""
+                    SELECT * FROM EspecialChannel WHERE type = %s
+                    """,
+                    ('welcome',)
+                )
+
+                result = cursor.fetchall()
+
+                if not result:
+                    cursor.execute("""
+                        INSERT INTO EspecialChannel (guild_id,channel_id,type,name) VALUES (
+                            %s,%s,%s,%s
+                        )
+                    """, (message.guild.id, message.channel.id, 'welcome', message.channel.name))
+
+                    conn.commit()
+                else:
+                    id_especial_channel = result[0][0]
+
+                    cursor.execute("""
+                        UPDATE EspecialChannel
+                        SET guild_id = %s,
+                            channel_id = %s,
+                            name = %s
+                        WHERE
+                            id = %s
+                    """, (message.guild.id, message.channel.id, message.channel.name, id_especial_channel))
+
+                    conn.commit()
+
+                await message.channel.send("Channel has been set as welcome channel")
+                break
 
 
 client.run(TOKEN)
